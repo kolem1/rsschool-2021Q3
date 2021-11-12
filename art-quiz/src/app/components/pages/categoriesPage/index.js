ba08aea88a970ff8f6d6a9d603d976a5504cffd6
Konstantin Lemko <lemko99@bk.ru>
2021-11-12 18:18:50 +0300

    feat: add category page and finish categories page

--- art-quiz/src/app/components/pages/categoriesPage/index.js
+++ art-quiz/src/app/components/pages/categoriesPage/index.js
@@ -1,5 +1,7 @@
 import Component from 'core/component';
 import Header from '../../common/header';
+import data from '../../common/data';
+import currentGame from '../../currentGame';
 
 import './styles.scss';
 import template from './template.html';
@@ -14,6 +16,38 @@ const categoriesPage = new Component({
       }
     })
   ],
+  events: {
+    '': ''
+  },
+  afterInitFunction: renderCategories
 });
 
 export default categoriesPage;
+
+async function renderCategories() {
+  const categoriesGrid = document.getElementById('categories-grid');
+  const topic = currentGame.topic;
+
+  const pictures = (await data.formatedArray)[topic];
+  console.log(pictures);
+  
+  pictures.forEach((item, i) => {
+    const category = document.createElement('article');
+    category.classList.add('category');
+    category.href = `#${topic}/category/${i}`;
+    category.innerHTML = `
+    <a class="category__link" href="#${topic}/game/${i}">
+      <div class="category__header">
+        <div class="category__counter"></div>
+        <h3 class="category__title">Round ${i + 1}</h3>
+      </div>
+      <div class="category__img-wrapper">
+        <img src="" alt="" class="category__img">
+      </div>
+    </a>
+    <a class="category__score" href="#${topic}/category/${i}">Score</a>
+    `;
+
+    categoriesGrid.append(category)
+  }) 
+}
--- art-quiz/src/app/components/routes.js
+++ art-quiz/src/app/components/routes.js
@@ -1,4 +1,6 @@
 import categoriesPage from './pages/categoriesPage';
+import categoryPage from './pages/categoryPage';
+import gamePage from './pages/gamePage';
 import homePage from './pages/homePage';
 import settingsPage from './pages/settingsPage';
 
@@ -20,5 +22,16 @@ const routes = [
     component: categoriesPage,
   },
 ];
+
+for(let i = 0; i < 12; i++) {
+  routes.push({
+    path: `artists/category/${i}`,
+    component: categoryPage,
+  });
+  routes.push({
+    path: `pictures/category/${i}`,
+    component: categoryPage,
+  });
+}
 
 export default routes;
