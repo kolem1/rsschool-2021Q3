be71b51c8b69d8443d323c2cfc2e63adbf70a06c
Konstantin Lemko <lemko99@bk.ru>
2021-11-21 14:15:06 +0300

    feat: add images preload for score page

--- art-quiz/src/app/views/pages/scorePage/functions/renderQuestions.js
+++ art-quiz/src/app/views/pages/scorePage/functions/renderQuestions.js
@@ -11,16 +11,21 @@ export default async function renderQuestions() {
   const results = JSON.parse(localStorage.getItem('kolem1-results'))[topic][round];
 
   pictures.forEach((picture, i) => {
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/kolem1/image-data/master/img/${picture.imageNum}.jpg`;
     const question = document.createElement('article');
     question.classList.add('question');
     if (results[i]) {
       question.classList.add('question--answered');
     }
     question.href = `#${topic}/category/${i}`;
+
+    img.onload = () => {
+      const imgWrapper = question.querySelector('.question__img-wrapper');
+      imgWrapper.innerHTML = `<img src="https://raw.githubusercontent.com/kolem1/image-data/master/img/${picture.imageNum}.jpg" alt="" class="question__img">`;
+    }
     question.innerHTML = `
-    <div class="question__img-wrapper">
-      <img src="https://raw.githubusercontent.com/kolem1/image-data/master/img/${picture.imageNum}.jpg" alt="" class="question__img">
-    </div>
+    <div class="question__img-wrapper"></div>
     <div class="question__info">
       <div class="question__name">${picture.name}</div>
       <div class="question__author">${picture.author}, ${picture.year}</div>
