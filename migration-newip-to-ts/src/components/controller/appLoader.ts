964fad416d49d9d65309233f7aba5d046884c111
Konstantin Lemko <lemko99@bk.ru>
2021-12-02 22:01:36 +0300

    feat: add API key

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '5fa8b00ed491453f9000b022c75faec9', // получите свой ключ https://newsapi.org/
         });
     }
 }
