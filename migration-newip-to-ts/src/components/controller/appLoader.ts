f448344f62b81d07af2e13c4dd938601add13380
Konstantin Lemko <lemko99@bk.ru>
2021-12-02 23:06:12 +0300

    refactor: adjust formating according to eslint

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '5fa8b00ed491453f9000b022c75faec9', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '5fa8b00ed491453f9000b022c75faec9', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
