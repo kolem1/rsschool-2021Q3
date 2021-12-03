93bc90f921d4d81bc2593febb30ef892efb12060
Konstantin Lemko <lemko99@bk.ru>
2021-12-03 15:48:44 +0300

    feat: type Appview class

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,18 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import IArticles from '../../interfaces/articles';
+import ISources from './../../interfaces/sources';
 
 export class AppView {
-  constructor() {
-    this.news = new News();
-    this.sources = new Sources();
-  }
+  private news: News = new News()
+  private sources: Sources = new Sources();
 
-  drawNews(data) {
+  drawNews(data: IArticles) {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data) {
+  drawSources(data: ISources) {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
