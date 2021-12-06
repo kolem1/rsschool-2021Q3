9158a5355758b2626b27c1d799c6d2c5b5639758
Konstantin Lemko <lemko99@bk.ru>
2021-12-06 21:37:20 +0300

    fix: change api link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: '5fa8b00ed491453f9000b022c75faec9', // получите свой ключ https://newsapi.org/
     });
   }
