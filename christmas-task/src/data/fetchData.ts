6fa81334d47f5cd1f983e2fb77bf9640e74f15ba
Konstantin Lemko <lemko99@bk.ru>
2021-12-18 19:43:17 +0300

    refactor: add class Fetcher

--- christmas-task/src/data/fetchData.ts
+++ christmas-task/src/data/fetchData.ts
@@ -1,5 +1,7 @@
-export async function fetchData<DataType>(url: string): Promise<DataType> {
-  const response = await fetch(url);
-  const data = (await response.json()) as Promise<DataType>;
-  return data;
+export class Fetcher {
+  static async fetchData<DataType>(url: string): Promise<DataType> {
+    const response = await fetch(url);
+    const data = (await response.json()) as Promise<DataType>;
+    return data;
+  }
 }
--- christmas-task/src/data/getToysData.ts
+++ christmas-task/src/data/getToysData.ts
@@ -1,8 +1,8 @@
-import { fetchData } from './fetchData';
+import { Fetcher } from './fetchData';
 import { IToy } from '../types';
 
 export default async function getToysData(setState: (data: IToy[]) => void): Promise<void> {
   const jsonUrl = process.env.REACT_APP_TOYS_URL as string;
-  const toys = await fetchData<IToy[]>(jsonUrl);
+  const toys = await Fetcher.fetchData<IToy[]>(jsonUrl);
   setState(toys);
 }
