e60428dc2d321421e911521ca298ffe60229451f
Konstantin Lemko <lemko99@bk.ru>
2021-12-21 02:01:46 +0300

    feat: add styles and animation for note about empty search

--- christmas-task/src/pages/Toys/Toys.tsx
+++ christmas-task/src/pages/Toys/Toys.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { MainContext } from '../../App';
 import { Filter, ToysList } from '../../components';
+import LottieAnimation from '../../components/LottieAnimation/LottieAnimation';
+import './Toys.css';
 
 export const Toys: React.FC = function () {
   const { toysData } = useContext(MainContext);
@@ -11,7 +13,17 @@ export const Toys: React.FC = function () {
           <Filter />
         </div>
         <div className="toys-page__list">
-          {toysData && (toysData.length ? <ToysList toysData={toysData} /> : 'Таких игрушек не найдено')}
+          {toysData &&
+            (toysData.length ? (
+              <ToysList toysData={toysData} />
+            ) : (
+              <div className="toys-page__empty">
+                <div className="toys-page__animation">
+                  <LottieAnimation name="gifts" path={`${process.env.PUBLIC_URL}/gifts.json`} />
+                </div>
+                <b>К сожалению, ничего не найдено :(</b>
+              </div>
+            ))}
         </div>
       </div>
     </div>
