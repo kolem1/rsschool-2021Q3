c70ba6506dc5d0a9121ff0cbb0268ebd1d6709ce
Konstantin Lemko <lemko99@bk.ru>
2022-01-24 21:43:16 +0300

    feat: add styles for winners page

--- async-race/src/pages/Winners/Winners.tsx
+++ async-race/src/pages/Winners/Winners.tsx
@@ -2,8 +2,10 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchWinners, setSortAndOrder, setWinnersPage } from '../../store/actions/winnersActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { Container, CarSvg } from '../../components';
+import { CarSvg, Page, PageTitle, PageCounter } from '../../components';
 import { OrderType, SortType } from '../../types/winners';
+import { Pagination } from './../../components/Pagination/Pagination';
+import styles from './Winners.module.css';
 
 export const Winners = () => {
   const { page, winners, total, sort, order } = useTypedSelector((state) => state.winners);
@@ -19,31 +21,31 @@ export const Winners = () => {
   }, [page, sort, order]);
 
   return (
-    <div>
-      <Container>
-        <h1>Winners ({total})</h1>
-        <h2>
-          Page {page} {totalPages > 1 && `from ${totalPages}`}
-        </h2>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th
+    <Page>
+      <PageTitle>Winners ({total})</PageTitle>
+      <PageCounter>
+        Page {page} {totalPages > 1 && `from ${totalPages}`}
+      </PageCounter>
+      <div>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>
+                <button
+                  className={styles.button}
+                  disabled={sort === SortType.id}
                   onClick={() => {
-                    let newOrder = OrderType.ASC;
-                    if (sort === SortType.id) {
-                      newOrder = order === OrderType.ASC ? OrderType.DESC : OrderType.ASC;
-                    }
-                    dispatch(setSortAndOrder(SortType.id, newOrder));
+                    dispatch(setSortAndOrder(SortType.id, OrderType.ASC));
                   }}
-                  style={{ cursor: 'pointer' }}
                 >
                   Number
-                </th>
-                <th>Car</th>
-                <th>Name</th>
-                <th
+                </button>
+              </th>
+              <th>Car</th>
+              <th>Name</th>
+              <th className={styles.winsCol}>
+                <button
+                  className={styles.button}
                   onClick={() => {
                     let newOrder = OrderType.ASC;
                     if (sort === SortType.wins) {
@@ -51,11 +53,13 @@ export const Winners = () => {
                     }
                     dispatch(setSortAndOrder(SortType.wins, newOrder));
                   }}
-                  style={{ cursor: 'pointer' }}
                 >
-                  Wins
-                </th>
-                <th
+                  Wins {sort === SortType.wins && (order === OrderType.ASC ? '↑' : '↓')}
+                </button>
+              </th>
+              <th className={styles.timeCol}>
+                <button
+                  className={styles.button}
                   onClick={() => {
                     let newOrder = OrderType.ASC;
                     if (sort === SortType.time) {
@@ -63,48 +67,40 @@ export const Winners = () => {
                     }
                     dispatch(setSortAndOrder(SortType.time, newOrder));
                   }}
-                  style={{ cursor: 'pointer' }}
                 >
-                  Best Time (seconds)
-                </th>
+                  Best Time (sec)
+                  {sort === SortType.time && (order === OrderType.ASC ? '↑' : '↓')}
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {winners.map((winner, i) => (
+              <tr key={winner.id}>
+                <td>{i + 1}</td>
+                <td>
+                  <CarSvg color={winner.color} />
+                </td>
+                <td>{winner.name}</td>
+                <td>{winner.wins}</td>
+                <td>{winner.time}</td>
               </tr>
-            </thead>
-            <tbody>
-              {winners.map((winner, i) => (
-                <tr key={winner.id}>
-                  <td>{i + 1}</td>
-                  <td>
-                    <CarSvg color={winner.color} />
-                  </td>
-                  <td>{winner.name}</td>
-                  <td>{winner.wins}</td>
-                  <td>{winner.time}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <button
-            disabled={page === 1 || total === 0}
-            onClick={() => {
-              dispatch(setWinnersPage(page - 1));
-              dispatch(fetchWinners(page));
-            }}
-          >
-            prev
-          </button>
-          <button
-            disabled={page === totalPages || total === 0}
-            onClick={async () => {
-              dispatch(setWinnersPage(page + 1));
-              dispatch(fetchWinners(page));
-            }}
-          >
-            Next
-          </button>
-        </div>
-      </Container>
-    </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        disabled={{
+          prev: page === 1 || total === 0,
+          next: page === totalPages || total === 0
+        }}
+        handleNextClick={() => {
+          dispatch(setWinnersPage(page + 1));
+        }}
+        handlePrevClick={() => {
+          dispatch(setWinnersPage(page - 1));
+        }}
+      />
+    </Page>
   );
 };
